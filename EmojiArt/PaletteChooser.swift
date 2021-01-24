f693c6636082b1957d18b1d8100b60ff14d3e3b0
DT Pham <dt99.pham@gmail.com>
2021-01-23 21:34:29 -0800

    Implemented palette chooser.

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -28,7 +28,8 @@ struct PaletteChooser: View {
                     showPaletteEditor = true
                 }
                 .popover(isPresented: $showPaletteEditor, content: {
-                    PaletteEditor(chosenPalette: $chosenPalette).environmentObject(document)
+                    PaletteEditor(chosenPalette: $chosenPalette, isShowing: $showPaletteEditor)
+                        .environmentObject(document)
                         .frame(minWidth: 300, minHeight: 500)
                 })
         }
@@ -39,12 +40,58 @@ struct PaletteChooser: View {
 struct PaletteEditor: View {
     @EnvironmentObject var document: EmojiArtDocument
     @Binding var chosenPalette: String
+    @State private var paletteName: String = ""
+    @State private var emojisToAdd: String = ""
+    @Binding var isShowing: Bool
+    
     var body: some View {
         VStack {
-            Text("Palette Editor").font(.headline)
+            ZStack {
+                Text("Palette Editor").font(.headline).padding()
+                HStack {
+                    Spacer()
+                    Button {
+                        isShowing = false
+                    } label: {
+                        Text("Done").padding()
+                    }
+                }
+            }
             Divider()
-            Text(document.paletteNames[chosenPalette] ?? "")
-            Text(chosenPalette)
+            Form {
+                Section {
+                    TextField("Palette Name", text: $paletteName) { began in
+                        if !began {
+                            document.renamePalette(chosenPalette, to: paletteName)
+                        }
+                    }
+                    TextField("Add Emoji", text: $emojisToAdd) { began in
+                        if !began {
+                            chosenPalette = document.addEmoji(emojisToAdd, toPalette: chosenPalette)
+                            emojisToAdd = ""
+                        }
+                    }
+                }
+                Section {
+                    Grid(chosenPalette.map{ String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font(Font.system(size: fontSize))
+                            .onTapGesture {
+                                chosenPalette = document.removeEmoji(emoji, fromPalette: chosenPalette)
+                            }
+                    }
+                    .frame(height: height)
+                }
+            }
         }
+        .onAppear {
+            paletteName = document.paletteNames[chosenPalette] ?? ""
+        }
+    }
+    
+    // MARK: - Drawing constants
+    private let fontSize: CGFloat = 40
+    private var height: CGFloat {
+        CGFloat((chosenPalette.count - 1) / 6) * 70 + 70
     }
 }
